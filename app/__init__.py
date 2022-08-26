95caba7bd3dc548d84aac50c78c29eef9d23a8ba
Konstantin-create <schahov.konst@gmail.com>
2022-08-26 20:55:47 +0300

    Linked the github dice on the about page to the GithubAPI

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
+import threading
 from flask import Flask, render_template
-
+from app.modules.github_tools import set_profile_data
 
 app = Flask(__name__)
 
+#  Start thread with GitHub statistic parser
+threading.Thread(target=set_profile_data).start()
+
 from app.routes import *
 
-# app.run(host='0.0.0.0', port=8000)
-
+app.run()
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,12 @@
+from app.modules import github_tools
 from app import app, render_template
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/about')
 def about():
-    return render_template('about.html')
-
+    return render_template('about.html', gh_stat=github_tools.get_profile_data())
