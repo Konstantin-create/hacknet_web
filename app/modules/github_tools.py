b9ca1e6eaa8a8f1db763c5e88771b8357f7e6a26
Konstantin-create <schahov.konst@gmail.com>
2022-09-07 21:44:16 +0300

    Worked on backend

--- app/modules/github_tools.py
+++ app/modules/github_tools.py
@@ -2,6 +2,15 @@ from github import Github
 
 token = open('TOKEN.txt', 'r').read()
 
-g = Github()
+g = Github(token)
 
-print(g.get_user())
+
+def get_followers():
+    return g.get_user().followers
+
+
+def get_repos():
+    repos = []
+    for repo in g.get_user().get_repos():
+        repos.append(repo.full_name)
+    return len(repos)
