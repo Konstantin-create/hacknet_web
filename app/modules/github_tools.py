fcb866fe42774ecc585ed339994c1b106ea6b35c
Konstantin-create <schahov.konst@gmail.com>
2022-09-08 07:40:31 +0300

    Worked on backend(github API)

--- app/modules/github_tools.py
+++ app/modules/github_tools.py
@@ -5,12 +5,30 @@ token = open('TOKEN.txt', 'r').read()
 g = Github(token)
 
 
-def get_followers():
+def get_statistics():
+    pass
+
+
+def get_followers() -> int:
+    """Function to count my followers"""
+
     return g.get_user().followers
 
 
-def get_repos():
+def get_repos() -> int:
+    """Function to count my repos"""
+
     repos = []
     for repo in g.get_user().get_repos():
         repos.append(repo.full_name)
     return len(repos)
+
+
+def get_stars() -> int:
+    """Function to count total stars"""
+
+    repos = list(g.get_user().get_repos())
+    total = 0
+    for repo in repos:
+        total += repo.stargazers_count
+    return total
