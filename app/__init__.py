f3bb9b2afda297d8fa733e856ece9600032281d9
Konstantin-create <schahov.konst@gmail.com>
2022-09-11 21:35:38 +0300

    create: parser for github profile readme. edit comments

--- app/__init__.py
+++ app/__init__.py
@@ -4,14 +4,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-print(github_tools.get_user_description())
 threading.Thread(target=github_tools.set_statistics).start()
 threading.Thread(target=github_tools.set_pinned_repos).start()
+threading.Thread(target=github_tools.set_user_description).start()
 
 
 @app.route('/')
 def init_page():
     return render_template(
         'index.html', gh_stat=github_tools.get_statistic(),
-        gh_pinned=github_tools.get_pinned_repos()
+        gh_pinned=github_tools.get_pinned_repos(),
+        description=github_tools.get_user_description()
     )
