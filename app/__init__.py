341163f9c4d13264f6ad463628aaf3029eabdeec
Konstantin-create <schahov.konst@gmail.com>
2022-09-25 16:29:01 +0300

    Worked on blog page

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
+import os
 import threading
 from app.modules import github_tools
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response
 
 app = Flask(__name__)
+web_site_folder = os.path.dirname(__file__)
 
 threading.Thread(target=github_tools.set_statistics).start()
 threading.Thread(target=github_tools.set_pinned_repos).start()
@@ -13,8 +15,10 @@ threading.Thread(target=github_tools.set_user_description).start()
 def init_page():
     return render_template(
         'index.html', gh_stat=github_tools.get_statistic(),
-        gh_pinned=github_tools.get_pinned_repos()
+        gh_pinned=github_tools.get_pinned_repos(),
+        web_site_folder=web_site_folder
     )
+
 
 @app.route('/blog')
 def blog_page():
