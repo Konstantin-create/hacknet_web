d816bf0c562a29448b11841e7a2d7a4e9fd3d191
Konstantin-create <schahov.konst@gmail.com>
2022-10-01 20:08:11 +0300

    start working on dashboard backend

---
+++ app/modules/dashboard_tools.py
@@ -0,0 +1,22 @@
+def get_visitors_list() -> list:
+    """Function to get visitors list from local file"""
+
+    ...
+
+
+def add_visitor_to_list() -> bool:
+    """Function to add visitor to list in local file"""
+
+    ...
+
+
+def get_day_statistics() -> dict:
+    """Function to get visits statistics by 24 hours"""
+
+    ...
+
+
+def get_month_statistics() -> dict:
+    """Function to get visits statistics by 1 month"""
+
+    ...
