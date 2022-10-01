df36ce65f5711d4d56f7a036328f4dab3d4adb5c
Konstantin-create <schahov.konst@gmail.com>
2022-10-01 21:00:52 +0300

    start working on dashboard backend

--- app/modules/dashboard_tools.py
+++ app/modules/dashboard_tools.py
@@ -1,13 +1,28 @@
+import json
+import os
+
+
 def get_visitors_list() -> list:
     """Function to get visitors list from local file"""
 
-    ...
+    if os.path.exists('app/data/visits/uniq.json'):
+        try:
+            return json.load(open('app/data/visits/uniq.json', 'r'))
+        except:
+            return []
+    return []
 
 
-def add_visitor_to_list() -> bool:
+def add_visitor_to_list(visitor: dict) -> bool:
     """Function to add visitor to list in local file"""
 
-    ...
+    visitors = get_visitors_list()
+    visitors.append(visitor)
+    try:
+        json.dump(visitors, open('app/data/visitors/uniq.json'))
+        return False
+    except:
+        return False
 
 
 def get_day_statistics() -> dict:
