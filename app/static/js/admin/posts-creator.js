3389cea7c15ea28c99d829c83fc48629470f7851
Konstantin-create <schahov.konst@gmail.com>
2022-10-14 07:23:34 +0300

    changefirst screen in posts creator page

--- app/static/js/admin/posts-creator.js
+++ app/static/js/admin/posts-creator.js
@@ -1,7 +1,7 @@
 let active_window = 'old'
 document.getElementById('img-3').className = 'aside-img active-page'
 document.getElementById('header-nav-1').className = 'header-nav-item active'
-document.getElementById('new-posts').className = 'new-posts hide-item'
+document.getElementById('old-posts').className = 'old-posts hide-item'
 let numOfPosts = document.getElementsByClassName('post').length;
 document.getElementsByTagName('main')[0].style.height = (numOfPosts * 500 + 250).toString() + 'px'
 document.getElementsByTagName('aside')[0].style.height = (numOfPosts * 500 + 250).toString() + 'px'
