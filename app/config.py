551dc5ce4e0aa69a7c4dce657ebc2c683952f659
Konstantin-create <schahov.konst@gmail.com>
2022-10-18 15:02:03 +0300

    add config file

---
+++ app/config.py
@@ -0,0 +1,7 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SQL_ALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///'
+    SQL_ALCHEMY_TEACK_MODIFICATIONS = False
