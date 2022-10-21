8ce3fb95fce7a1968b5210fe5b1b9fc620253dcf
Konstantin-create <schahov.konst@gmail.com>
2022-10-21 15:32:37 +0300

    worked on create post form

--- app/static/js/admin/posts-creator.js
+++ app/static/js/admin/posts-creator.js
@@ -8,6 +8,8 @@ document.getElementById('img-3').className = 'aside-img active-page'
 document.getElementById('header-nav-1').className = 'header-nav-item active'
 document.getElementById('old-posts').className = 'old-posts hide-item'
 
+document.getElementsByTagName('main')[0].style.height = '200vh'
+document.getElementsByTagName('aside')[0].style.height = '200vh'
 
 for (let i = 1; i <= form_screens.length; i++) {
     document.getElementById(form_screens[i]).className = form_screens[i] + ' hidden'
@@ -20,8 +22,8 @@ function change_active() {
         document.getElementById('header-nav-2').className = 'header-nav-item active'
         document.getElementById('old-posts').className = 'old-posts hide-item'
         document.getElementById('new-posts').className = 'new-posts'
-        document.getElementsByTagName('main')[0].style.height = '100vh'
-        document.getElementsByTagName('aside')[0].style.height = '100vh'
+        document.getElementsByTagName('main')[0].style.height = '200vh'
+        document.getElementsByTagName('aside')[0].style.height = '200vh'
     } else {
         active_window = 'old'
         document.getElementById('header-nav-1').className = 'header-nav-item active'
