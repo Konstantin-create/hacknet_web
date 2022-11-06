de8fca2d1f7049b9e8a57da3ac20434a1b0268c4
Konstantin-create <schahov.konst@gmail.com>
2022-11-06 19:37:57 +0300

    worked on content editor backend

--- app/modules/content_editor.py
+++ app/modules/content_editor.py
@@ -2,6 +2,12 @@ import json
 
 
 def edit_content(new_data: dict):
-    """Function to edit content configs"""
+    """Function to edit content config file"""
 
-    return
+    json.dump(new_data, open('app/data/content.json', 'w'))
+
+
+def get_content() -> dict:
+    """Function to get content from config file"""
+
+    return json.load(open('app/data/content.json', 'r'))
