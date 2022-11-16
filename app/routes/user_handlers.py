8625bd17898e5369b153412968a0fc95ad71ce5a
Konstantin-create <schahov.konst@gmail.com>
2022-11-16 10:40:07 +0200

    worked on likes backend

---
+++ app/routes/user_handlers.py
@@ -0,0 +1,15 @@
+from app import db
+from app import app, redirect
+from app.modules.models import Posts
+
+@app.route('/user/add-post-like/<int:post_id>')
+def user_add_like(post_id: int):
+
+    try:
+        post = Posts.query.get(post_id)
+        post.likes += 1
+        db.session.add(post)
+        db.session.commit()
+    except:
+        pass
+    return redirect(f'/posts/{post_id}')
