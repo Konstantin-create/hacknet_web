cb690e160022ced511f416a358bd0ff524606ada
Konstantin-create <schahov.konst@gmail.com>
2022-11-16 10:43:37 +0200

    worked at dislike backend

--- app/routes/user_handlers.py
+++ app/routes/user_handlers.py
@@ -13,3 +13,15 @@ def user_add_like(post_id: int):
     except:
         pass
     return redirect(f'/posts/{post_id}')
+
+@app.route('/user/add-post-like/<int:post_id>')
+def user_add_dislike(post_id: int):
+
+    try:
+        post = Posts.query.get(post_id)
+        post.dislikes += 1
+        db.session.add(post)
+        db.session.commit()
+    except:
+        pass
+    return redirect(f'/posts/{post_id}')
