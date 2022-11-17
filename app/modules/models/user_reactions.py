15ef5e8ac2875d4b415bcb33184ac440080c5ae4
Konstantin-create <schahov.konst@gmail.com>
2022-11-17 21:48:04 +0300

    create classes for like and dislike tables in db

---
+++ app/modules/models/user_reactions.py
@@ -0,0 +1,19 @@
+from app import db
+
+
+class Likes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
+    user_ip = db.Column(db.String(128), nullable=False)
+
+    def __repr__(self):
+        return f'<Like: {self.user_ip} | {self.post_id}>'
+
+
+class Dislikes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
+    user_ip = db.Column(db.String(128), nullable=False)
+
+    def __repr__(self):
+        return f'<Dislike: {self.user_ip} | {self.post_id}>'
